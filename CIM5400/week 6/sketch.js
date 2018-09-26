45eb917e82a878e8c35c8131563067fbe74e23da
cole <CHK29@miami.edu>
2018-09-26 12:22:15 -0400

    everythign

---
+++ CIM5400/week 6/sketch.js
@@ -0,0 +1,23 @@
+function setup() {
+  createCanvas(480, 120);
+  textSize(64);
+  textALign(center)
+  filll(255);
+
+}
+function draw(){
+background(0);
+if(keyispressed) {
+  text(key, 60, 80)
+}
+function draw(){
+text('A new hope', 20, 20);
+text('it is a period of civil war', 20, 20)
+text('Rebel spaceships, striking', 40, 40)
+text('from a hidden base, have won', 60, 60)
+text('their first victory against the evil', 80, 80)
+text('Galactic empire...', 100, 100)
+
+}
+
+}
